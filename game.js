8050be788fa4429c03b3bf3e3e04e4fbbefbad83
Douglas Fessler <133989704+fessler2023@users.noreply.github.com>
2026-01-14 13:56:44 -0500

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: '#5DADE2', // light blue stream
+    backgroundColor: '#5DADE2', // light blue = stream/lake
     physics: {
         default: 'arcade',
         arcade: {
@@ -20,66 +20,64 @@ const game = new Phaser.Game(config);
 
 let player;
 let cursors;
-let bugs = [];
+let rocks = [];
+let collectedSpecies = [];
 let score = 0;
 let scoreText;
 
 function preload() {
-    // Load your PNG for player and bug
-    this.load.image('player', 'bug.png'); // reuse your PNG for now
-    this.load.image('bug', 'bug.png');    // bug sprite
-    // Simple bush placeholder (any small image URL works)
-    this.load.image('bush', 'https://i.imgur.com/3k0Q0KX.png');
+    // Placeholder images
+    this.load.image('player', 'player.png'); // your little guy
+    this.load.image('rock', 'rock.png');     // rock you flip
+    this.load.image('species', 'bug.png');   // species hidden under rock
 }
 
 function create() {
-    // Add some “bushes/rocks” as obstacles
-    this.add.image(200, 150, 'bush').setScale(0.5);
-    this.add.image(600, 400, 'bush').setScale(0.5);
-    this.add.image(400, 300, 'bush').setScale(0.5);
-
-    // Add player in center, scale down
+    // Add player in center
     player = this.physics.add.sprite(400, 500, 'player').setScale(0.3);
     player.setCollideWorldBounds(true);
-
-    // Add multiple bugs randomly, scaled smaller
-    for (let i = 0; i < 5; i++) {
-        const x = Phaser.Math.Between(50, 750);
-        const y = Phaser.Math.Between(50, 550);
-        let bug = this.physics.add.sprite(x, y, 'bug').setInteractive().setScale(0.15);
-        bugs.push(bug);
-
-        // Click to catch bug
-        bug.on('pointerdown', () => {
-            score += 10;
-            scoreText.setText('Score: ' + score);
-            bug.destroy(); // bug disappears after catch
-        });
-    }
 
     // Add score display
     scoreText = this.add.text(10, 10, 'Score: 0', { font: '20px Arial', fill: '#fff' });
 
-    // Enable arrow key movement
     cursors = this.input.keyboard.createCursorKeys();
+
+    // Add rocks at specific positions
+    const rockPositions = [
+        {x: 200, y: 200},
+        {x: 400, y: 300},
+        {x: 600, y: 250},
+    ];
+
+    rockPositions.forEach((pos, index) => {
+        const rock = this.physics.add.sprite(pos.x, pos.y, 'rock').setScale(0.3).setInteractive();
+        rocks.push(rock);
+
+        rock.on('pointerdown', () => {
+            // Only allow collection if player is close
+            if (Phaser.Math.Distance.Between(player.x, player.y, rock.x, rock.y) < 60) {
+                // spawn species on top of rock
+                const species = this.add.sprite(rock.x, rock.y, 'species').setScale(0.2);
+                collectedSpecies.push('species'+(index+1)); // store species key
+                score += 10;
+                scoreText.setText('Score: ' + score);
+                rock.destroy();
+            } else {
+                // optional: show message “get closer to flip the rock”
+                console.log('Move closer to flip the rock!');
+            }
+        });
+    });
 }
 
 function update() {
     // Reset velocity
     player.setVelocity(0);
-
     const speed = 200;
 
-    if (cursors.left.isDown) {
-        player.setVelocityX(-speed);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(speed);
-    }
+    if (cursors.left.isDown) player.setVelocityX(-speed);
+    else if (cursors.right.isDown) player.setVelocityX(speed);
 
-    if (cursors.up.isDown) {
-        player.setVelocityY(-speed);
-    } else if (cursors.down.isDown) {
-        player.setVelocityY(speed);
-    }
+    if (cursors.up.isDown) player.setVelocityY(-speed);
+    else if (cursors.down.isDown) player.setVelocityY(speed);
 }
-
