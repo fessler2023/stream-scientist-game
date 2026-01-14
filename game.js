cc50cb7e5faf230911d967c23589ebb1c0db14c0
Douglas Fessler <133989704+fessler2023@users.noreply.github.com>
2026-01-14 14:32:10 -0500

    Update game.js

--- game.js
+++ game.js
@@ -50,7 +50,7 @@ function create() {
     ];
 
     rockPositions.forEach((pos, index) => {
-        const rock = this.physics.add.sprite(pos.x, pos.y, 'rock').setScale(0.3).setInteractive();
+        const rock = this.physics.add.sprite(pos.x, pos.y, 'rock').setScale(2).setInteractive();
         rocks.push(rock);
 
         rock.on('pointerdown', () => {
@@ -81,3 +81,4 @@ function update() {
     if (cursors.up.isDown) player.setVelocityY(-speed);
     else if (cursors.down.isDown) player.setVelocityY(speed);
 }
+
